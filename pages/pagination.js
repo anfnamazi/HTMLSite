65e8a792c83c6b575e2f68618adbafdbedee85d3
Ahmad Namazi <Ahmad Namazi>
2019-07-22 22:21:01 +0430

    edit 1.html js & css

--- pages/pagination.js
+++ pages/pagination.js
@@ -5,9 +5,7 @@ var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-	  showDivs(slideIndex += n);
-	  if(n==1){a.href="#"}
-	  if(n==-1){alert('-1')}
+	showDivs(slideIndex += n);
 }
 
 
@@ -19,15 +17,12 @@ function currentDiv(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "inline-block";  
+	x[i].className = x[i].className.replace(" green-btn", "");
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-cyan", "");
-  }
-  x[slideIndex-1].style.display = "inline-block";  
-  dots[slideIndex-1].className += " w3-cyan";
+  x[slideIndex-1].className =x[slideIndex-1].className+ " green-btn";
+  
 }
