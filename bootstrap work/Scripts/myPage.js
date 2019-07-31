bacb757b3a7571c10e021b1a7ef92cfea032731c
Ahmad Namazi <Ahmad Namazi>
2019-07-31 21:08:58 +0430

    add pagination &...

--- bootstrap work/Scripts/myPage.js
+++ bootstrap work/Scripts/myPage.js
@@ -1,3 +1,4 @@
+/*if for login page*/
 var a = document.getElementById('inputEmail3');var b=document.getElementById('inputPassword3');function sub(){if (a.value==''){alert('please enter email');document.getElementById('myform').action='myPage.html';}if(b.value==''){alert('enter password');document.getElementById('myform').action='myPage.html';}}
 /*dropdown btn sidebar*/
 var dropdown = document.getElementsByClassName("dropdown-btn");
@@ -13,4 +14,33 @@ for (i = 0; i < dropdown.length; i++) {
   dropdownContent.style.display = "block";
   }
   });
+}
+
+
+/*button pager*/
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+	showDivs(slideIndex += n);
+}
+
+
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "inline-block";  
+	x[i].className = x[i].className.replace(" green-btn", "");
+  }
+  x[slideIndex-1].className =x[slideIndex-1].className+ " green-btn";
+  
 }
