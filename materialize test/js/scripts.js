fc25ef4fa39bceecc4bedd9bf483a2d152bf7ba6
anfnamazi <anf.namazi@gmail.com>
2019-09-14 20:03:54 +0430

    add autocomplete...

--- materialize test/js/scripts.js
+++ materialize test/js/scripts.js
@@ -9,3 +9,19 @@ $(".carousel").carousel();
 $(window).on("load", function () {
     $('.tap-target').tapTarget('open');
 });
+//for autocomplete (firstName)
+$('input.autocomplete').keypress(function () {
+    $('input.autocomplete').autocomplete({
+        data: {
+            "Apple": 'images/apple.png',
+            "Microsoft": 'images/micro.jpg',
+            "Google": null
+        },
+        limit: 20,
+        onAutocomplete: function (val) {
+
+        },
+        minLength: 1,
+    });
+});
+//...................
